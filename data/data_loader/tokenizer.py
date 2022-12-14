251b5f916d6593b641fdabdc59081a06fc4a240e
stanxcluo <stanxcluo@tencent.com>
2022-12-15 02:33:47 +0800

    add

--- data/data_loader/tokenizer.py
+++ data/data_loader/tokenizer.py
@@ -6,5 +6,17 @@ class Tokenizer:
 	def __init__(self, name="bert-base-chinese"):
 		self._tok = BertTokenizer.from_pretrained(Str2TokenPath[name])
 
-	def __
+	def __call__(case, seq_length):
+		if isinstance(case, str):
+			return self._tok.encode_plus(case, max_length=seq_length, padding="max_length")
+		if isinstance(case, list):
+			_res = {}
+			for one in case:
+				tmp = self.__call__(one, seq_length)
+				for key, val in tmp:
+					if key not in _res:
+						_res[key] = []
+					_res[key].append(val)
+			return _res
+		raise Exception("input::case should be instance of str or list!")
 
