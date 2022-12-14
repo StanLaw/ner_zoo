9f4f5da591fd9b02c743860c7eba997cb729dd52
stanxcluo <stanxcluo@tencent.com>
2022-12-14 23:37:31 +0800

    add

--- models/pretrain/dump_bert_base.sh
+++ models/pretrain/dump_bert_base.sh
@@ -1,5 +1,12 @@
 
+target=bert-base-chinese
+
+mkdir $target
+cd $target
+
 wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese.tar.gz
 
+tar -zxvf bert-base-chinese.tar.gz
 
+wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese-vocab.txt
 
